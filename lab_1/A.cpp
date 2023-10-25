7272f325d8cafed019fb59b336aabfba00ba3fd4
Viktor825 <148805807+Viktor825@users.noreply.github.com>
2023-10-25 22:05:43 +0600

    Add files via upload

---
+++ lab_1/A.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+int main() {
+    double a, b;
+    std::cin >> a >> b;
+    double c = sqrt(a * a + b * b);
+    std::cout << c << std::endl;
+    return 0;
+}
